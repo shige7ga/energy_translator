5574285ad1ddd82e634e1e13940eff9399ee2ae6
shige7ga <ameft_80@yahoo.co.jp>
2026-05-03 20:32:13 +0900

    turbo_stream活用及びバリデーション実装

--- app/controllers/translators_controller.rb
+++ app/controllers/translators_controller.rb
@@ -1,13 +1,42 @@
 class TranslatorsController < ApplicationController
   def index
+    @form = TranslationForm.new
     @conversions = Conversion.all
   end
 
   def create
-    @input = params[:input].to_f
-    @conversion = Conversion.find(params[:conversion_id])
-    @output = @conversion.translate(@input)
+    @form = TranslationForm.new(translation_params)
 
-    render partial: "translators/shared/result"
+    if @form.valid?
+      render turbo_stream: [
+        turbo_stream.update('input_error', ''),
+        turbo_stream.update('conversion_id', ''),
+        turbo_stream.update(
+          "result_area",
+          partial: 'translators/shared/result',
+          locals: { output: @form.result }
+        )
+      ]
+    else
+      render turbo_stream: [
+        turbo_stream.update(
+          "input_error",
+          partial: "translators/shared/field_error",
+          locals: { form: @form, field: :input }
+        ),
+        turbo_stream.update(
+          "conversion_id_error",
+          partial: "translators/shared/field_error",
+          locals: { form: @form, field: :conversion_id }
+        ),
+        turbo_stream.update("result_area", "")
+      ], status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def translation_params
+    params.require(:translation_form).permit(:input, :conversion_id)
   end
 end
